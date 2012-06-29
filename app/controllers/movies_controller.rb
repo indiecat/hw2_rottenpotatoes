88258f2ca3af6969daf7f608a707613f8b466660
saasbook <saasbook@saasbook.(none)>
2012-06-29 10:25:32 -0700

    added dummy checkboxes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,6 +7,7 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @all_ratings = ['G','PG','PG- 13','R']
     sort_column = params[:sort].to_s
     @movies = Movie.order(sort_column).all
   end
